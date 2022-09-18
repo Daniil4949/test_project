44aef5bc26ead983982fc573ee760ee9b9f2c8b6
Daniil4949 <daniilkimstachp@gmail.com>
2022-09-18 10:47:03 +0300

    Classes for the categories and for the books were successfully created

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import MainPage, BookInfo
+from .views import MainPage, BookInfo, CategoryBook, CartInfo
 
 
 urlpatterns = [
     path('', MainPage.as_view(), name='home'),
-    # path('<slug:category_slug>/', CategoryBook.as_view(), name='category_books'),
-    # path('cart/', Cart.as_view(), name='cart'),
-    path('<slug:book_slug>/', BookInfo.as_view(), name='book')
+    path('categories/<slug:category_slug>/', CategoryBook.as_view(), name='category_books'),
+    path('cart/<str:username>', CartInfo.as_view(), name='cart'),
+    path('books/<slug:book_slug>/', BookInfo.as_view(), name='book')
 ]
--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
-from .models import Category, Book
+from .models import Category, Book, Cart
+from users.models import CustomUser
 
 
-#TODO: create all the classes for the cayegories and make it work in a right way
+# TODO: create all the classes for the cayegories and make it work in a right way
 
 
 class MainPage(ListView):
@@ -11,18 +12,26 @@ class MainPage(ListView):
     template_name = "menu/main_page.html"
 
 
-# class CategoryBook(DetailView):
-#     template_name = "menu/category_books_page.html"
-#     slug_url_kwarg = 'category_slug'
-#     context_object_name = 'books'
-#
+class CategoryBook(ListView):
+    template_name = "menu/category_books_page.html"
+    model = Book
+    context_object_name = 'books'
+    allow_empty = False
+
+    def get_queryset(self):
+        category = Category.objects.get(slug=self.kwargs['category_slug'])
+        return Book.objects.filter(category=category)
 
 
-# class Cart(DetailView):
-#     template_name = "menu/cart.html"
-#
-#     def get_queryset(self):
-#         return Cart.objects.filter(user=self.kwargs['user'])
+class CartInfo(ListView):
+    template_name = "menu/cart.html"
+    model = Cart
+    context_object_name = 'cart'
+    allow_empty = False
+
+    def get_queryset(self):
+        user = CustomUser.objects.get(email=self.kwargs['request.user.email'])
+        return Cart.objects.filter(user=user)
 
 
 class BookInfo(DetailView):
@@ -30,9 +39,5 @@ class BookInfo(DetailView):
     model = Book
     context_object_name = 'book'
     slug_url_kwarg = 'book_slug'
-
-    def get_queryset(self):
-        book_slug = self.kwargs['book_slug']
-        return Book.objects.get(slug=book_slug)
 
 # Create your views here.
