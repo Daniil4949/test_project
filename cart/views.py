a19a9ca8d78abbc8424d290ecf12c00652e162c2
Daniil4949 <daniilkimstachp@gmail.com>
2022-09-26 14:04:58 +0300

    Comments were added.

--- cart/views.py
+++ cart/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, redirect
 from books.models import Cart, Book
 from .forms import SearchBookForm
 
-#TODO: It is necessary to implement search function
-
 
 def cart(request):
+    """Returns all the products and it's quantity for each user"""
     cart = Cart.objects.filter(user=request.user)
     return render(request, "cart/cart.html", {"cart": cart})
 
 
 def add_to_cart(request, book_slug):
+    """Block try/except is necessary because we try to find book firstly, and after that
+    we add the selected book to the cart or increase books' quantity. if we do not use block try/except
+    it would not work in a correct way because we cannot find book that does not exist. We can use 'get_object_or_404'
+    instead.' """
     try:
         book = Book.objects.get(slug=book_slug)
         cart = Cart.objects.get(user=request.user, book=book)
@@ -23,6 +26,7 @@ def add_to_cart(request, book_slug):
 
 
 def delete_from_the_cart(request, book_slug):
+    """Deleting the definite book from the cart"""
     book = Book.objects.get(slug=book_slug)
     delete_list = Cart.objects.filter(user=request.user, book=book)
     delete_list.delete()
@@ -30,6 +34,7 @@ def delete_from_the_cart(request, book_slug):
 
 
 def delete_all(request):
+    """Deleting all the books from the cart"""
     delete_list = Cart.objects.filter(user=request.user)
     delete_list.delete()
     return redirect('cart')
@@ -40,7 +45,7 @@ def payment(request):
 
 
 def search_book(request):
-    """Form for searching books"""
+    """Form for searching books. It is working also with not the full title of the book."""
     search_form = SearchBookForm(request.POST)
     if request.method == 'POST':
         if search_form.is_valid():
--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    """it is custom user model with a unique email field"""
     username = models.CharField(max_length=20, null=False, unique=True)
     email = models.CharField(max_length=30, null=False, unique=True)
     password = models.TextField(unique=True, null=False)
