8f8a6532a14f4f8f4b031e08442df9019e3260ee
Daniil4949 <daniilkimstachp@gmail.com>
2022-09-29 10:14:43 +0300

    Model Payment was updated and form for buying books was added

--- cart/forms.py
+++ cart/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from books.models import Book
-
+from .models import Payment
 
 class SearchBookForm(forms.ModelForm):
     """Form for searching posts"""
@@ -9,3 +9,19 @@ class SearchBookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ('title',)
+
+
+class PaymentForm(forms.ModelForm):
+    number_of_card = forms.CharField(label='login', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    validity_period_month = forms.CharField(label='email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    validity_period_year = forms.CharField(label='Enter password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = Payment
+        fields = ('number_of_card', 'validity_period_month', 'validity_period_year', )
+        widgets = {
+            'number_of_card': forms.TextInput(attrs={'class': 'form-input'}),
+            'validity_period_month': forms.TextInput(attrs={'class': 'form-input'}),
+            'validity_period_year': forms.TextInput(attrs={'class': 'form-input'}),
+
+        }
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from books.models import Cart
 
+
+class Payment(models.Model):
+    number_of_cart = models.CharField(max_length=16)
+    validity_period_month = models.CharField(max_length=2)
+    validity_period_year = models.CharField(max_length=2)
+    purchased_book = models.ForeignKey(Cart, on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now=True)
 # Create your models here.
