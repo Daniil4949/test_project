c2d0c6d2bfa067c31e6d34de6133cff7545821b3
Daniil <daniilkimstachp@gmail.com>
2022-10-06 16:54:31 +0300

    Some more documentation

--- shop_api/serializers.py
+++ shop_api/serializers.py
@@ -2,6 +2,16 @@ from cart.models import Cart, Payment
 from books.models import Book, Rating, Author, Category, Comment
 from rest_framework import serializers
 
+"""
+Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that 
+can then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization, 
+allowing parsed data to be converted back into complex types, after first validating the incoming data.
+The serializers in REST framework work very similarly to Django's Form and ModelForm classes. We provide a 
+Serializer class which gives you a powerful, generic way to control the output of your responses, as well as 
+a ModelSerializer class which provides a useful shortcut for creating serializers that 
+deal with model instances and querysets.
+"""
+
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,7 +22,7 @@ class BookSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('name', )
+        fields = ('name',)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
